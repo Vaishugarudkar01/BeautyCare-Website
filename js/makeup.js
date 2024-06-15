64f8fbb5fc1e8965c3199556b366d1a23ee324b5
Vaishugarudkar01 <vaishugarudkar2005@gmail.com>
2024-06-15 12:12:15 +0530

    chnges in card

---
+++ js/makeup.js
@@ -0,0 +1,23 @@
+function slider1(){
+    const slideChanger = document.getElementById("slider");
+
+    slideChanger.src = '../images/lip1.avif' ;
+}
+function slider2(){
+    const slideChanger = document.getElementById("slider");
+
+    slideChanger.src = '../images/lip2.avif' ;
+
+}
+function slider3(){
+    const slideChanger = document.getElementById("slider");
+
+    slideChanger.src = '../images/lip3.avif' ;
+}
+function slider4(){
+    const slideChanger = document.getElementById("slider");
+
+    slideChanger.src = '../images/lip4.avif' ;
+}
+
+

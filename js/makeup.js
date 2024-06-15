42cb4109d1622dbfb3c27e5deeae29cd704a1d41
Vaishugarudkar01 <vaishugarudkar2005@gmail.com>
2024-06-15 14:31:30 +0530

    js add to cards

--- js/makeup.js
+++ js/makeup.js
@@ -21,3 +21,316 @@ function slider4(){
 }
 
 
+function incre(){
+    const quantityText = document.getElementById("quantity-text");
+    const total = document.getElementById("total");
+    const showquantity = document.getElementById("showqauntity")
+    let quantity = parseInt(quantityText.innerText);
+    if(quantity<10){
+    quantityText.innerText = ++quantity;
+    }
+    else{
+        alert('You cant order more than 10 items')
+    }
+    total.innerText =  `Total:${quantity*247}/-`;
+    showqauntity.innerText = `${quantity} x 247`;
+ }
+
+ function decre(){
+    const quantityText = document.getElementById("quantity-text");
+    const total = document.getElementById("total");
+    const showquantity = document.getElementById("showqauntity")
+    let quantity = parseInt(quantityText.innerText);
+    if(quantity>1){
+    quantityText.innerText = --quantity;
+    }
+    else{
+        alert('You cant order less than 1 items')
+    }
+    total.innerText =  `Total:${quantity*247}/-`;
+    showqauntity.innerText = `${quantity} x 247`;
+   
+ }
+
+// foundation  
+
+
+function slider8(){
+    const slideChanger = document.getElementById("slider1");
+
+    slideChanger.src = '../images/foundation1.avif' ;
+}
+function slider5(){
+    const slideChanger = document.getElementById("slider1");
+
+    slideChanger.src = '../images/foundation2.avif' ;
+
+}
+function slider6(){
+    const slideChanger = document.getElementById("slider1");
+
+    slideChanger.src = '../images/foundation3.avif' ;
+}
+function slider7(){
+    const slideChanger = document.getElementById("slider1");
+
+    slideChanger.src = '../images/foundation.webp' ;
+}
+
+
+function incre1(){
+    const quantityText = document.getElementById("quantity-text1");
+    const total = document.getElementById("total1");
+    const showquantity = document.getElementById("showqauntity1")
+    let quantity = parseInt(quantityText.innerText);
+    if(quantity<10){
+    quantityText.innerText = ++quantity;
+    }
+    else{
+        alert('You cant order more than 10 items')
+    }
+    total1.innerText =  `Total:${quantity*765}/-`;
+    showqauntity1.innerText = `${quantity} x 764`;
+ }
+
+ function decre1(){
+    const quantityText = document.getElementById("quantity-text1");
+    const total = document.getElementById("total1");
+    const showquantity = document.getElementById("showqauntity1")
+    let quantity = parseInt(quantityText.innerText);
+    if(quantity>1){
+    quantityText.innerText = --quantity;
+    }
+    else{
+        alert('You cant order less than 1 items')
+    }
+    total1.innerText =  `Total:${quantity*764}/-`;
+    showqauntity1.innerText = `${quantity} x 764`;
+   
+ }
+
+// kajal
+
+
+function slider8(){
+    const slideChanger = document.getElementById("slider1");
+
+    slideChanger.src = '../images/foundation1.avif' ;
+}
+function slider5(){
+    const slideChanger = document.getElementById("slider1");
+
+    slideChanger.src = '../images/foundation2.avif' ;
+
+}
+function slider6(){
+    const slideChanger = document.getElementById("slider1");
+
+    slideChanger.src = '../images/foundation3.avif' ;
+}
+function slider7(){
+    const slideChanger = document.getElementById("slider1");
+
+    slideChanger.src = '../images/foundation.webp' ;
+}
+
+
+function incre1(){
+    const quantityText = document.getElementById("quantity-text1");
+    const total = document.getElementById("total1");
+    const showquantity = document.getElementById("showqauntity1")
+    let quantity = parseInt(quantityText.innerText);
+    if(quantity<10){
+    quantityText.innerText = ++quantity;
+    }
+    else{
+        alert('You cant order more than 10 items')
+    }
+    total1.innerText =  `Total:${quantity*765}/-`;
+    showqauntity1.innerText = `${quantity} x 764`;
+ }
+
+ function decre1(){
+    const quantityText = document.getElementById("quantity-text1");
+    const total = document.getElementById("total1");
+    const showquantity = document.getElementById("showqauntity1")
+    let quantity = parseInt(quantityText.innerText);
+    if(quantity>1){
+    quantityText.innerText = --quantity;
+    }
+    else{
+        alert('You cant order less than 1 items')
+    }
+    total1.innerText =  `Total:${quantity*764}/-`;
+    showqauntity1.innerText = `${quantity} x 764`;
+   
+ }
+
+//  neil polish
+
+function slider8(){
+    const slideChanger = document.getElementById("slider1");
+
+    slideChanger.src = '../images/foundation1.avif' ;
+}
+function slider5(){
+    const slideChanger = document.getElementById("slider1");
+
+    slideChanger.src = '../images/foundation2.avif' ;
+
+}
+function slider6(){
+    const slideChanger = document.getElementById("slider1");
+
+    slideChanger.src = '../images/foundation3.avif' ;
+}
+function slider7(){
+    const slideChanger = document.getElementById("slider1");
+
+    slideChanger.src = '../images/foundation.webp' ;
+}
+
+
+function incre1(){
+    const quantityText = document.getElementById("quantity-text1");
+    const total = document.getElementById("total1");
+    const showquantity = document.getElementById("showqauntity1")
+    let quantity = parseInt(quantityText.innerText);
+    if(quantity<10){
+    quantityText.innerText = ++quantity;
+    }
+    else{
+        alert('You cant order more than 10 items')
+    }
+    total1.innerText =  `Total:${quantity*765}/-`;
+    showqauntity1.innerText = `${quantity} x 764`;
+ }
+
+ function decre1(){
+    const quantityText = document.getElementById("quantity-text1");
+    const total = document.getElementById("total1");
+    const showquantity = document.getElementById("showqauntity1")
+    let quantity = parseInt(quantityText.innerText);
+    if(quantity>1){
+    quantityText.innerText = --quantity;
+    }
+    else{
+        alert('You cant order less than 1 items')
+    }
+    total1.innerText =  `Total:${quantity*764}/-`;
+    showqauntity1.innerText = `${quantity} x 764`;
+   
+ }
+
+
+//  blush
+
+function slider8(){
+    const slideChanger = document.getElementById("slider1");
+
+    slideChanger.src = '../images/foundation1.avif' ;
+}
+function slider5(){
+    const slideChanger = document.getElementById("slider1");
+
+    slideChanger.src = '../images/foundation2.avif' ;
+
+}
+function slider6(){
+    const slideChanger = document.getElementById("slider1");
+
+    slideChanger.src = '../images/foundation3.avif' ;
+}
+function slider7(){
+    const slideChanger = document.getElementById("slider1");
+
+    slideChanger.src = '../images/foundation.webp' ;
+}
+
+
+function incre1(){
+    const quantityText = document.getElementById("quantity-text1");
+    const total = document.getElementById("total1");
+    const showquantity = document.getElementById("showqauntity1")
+    let quantity = parseInt(quantityText.innerText);
+    if(quantity<10){
+    quantityText.innerText = ++quantity;
+    }
+    else{
+        alert('You cant order more than 10 items')
+    }
+    total1.innerText =  `Total:${quantity*765}/-`;
+    showqauntity1.innerText = `${quantity} x 764`;
+ }
+
+ function decre1(){
+    const quantityText = document.getElementById("quantity-text1");
+    const total = document.getElementById("total1");
+    const showquantity = document.getElementById("showqauntity1")
+    let quantity = parseInt(quantityText.innerText);
+    if(quantity>1){
+    quantityText.innerText = --quantity;
+    }
+    else{
+        alert('You cant order less than 1 items')
+    }
+    total1.innerText =  `Total:${quantity*764}/-`;
+    showqauntity1.innerText = `${quantity} x 764`;
+   
+ }
+
+//  contour
+
+function slider8(){
+    const slideChanger = document.getElementById("slider1");
+
+    slideChanger.src = '../images/foundation1.avif' ;
+}
+function slider5(){
+    const slideChanger = document.getElementById("slider1");
+
+    slideChanger.src = '../images/foundation2.avif' ;
+
+}
+function slider6(){
+    const slideChanger = document.getElementById("slider1");
+
+    slideChanger.src = '../images/foundation3.avif' ;
+}
+function slider7(){
+    const slideChanger = document.getElementById("slider1");
+
+    slideChanger.src = '../images/foundation.webp' ;
+}
+
+
+function incre1(){
+    const quantityText = document.getElementById("quantity-text1");
+    const total = document.getElementById("total1");
+    const showquantity = document.getElementById("showqauntity1")
+    let quantity = parseInt(quantityText.innerText);
+    if(quantity<10){
+    quantityText.innerText = ++quantity;
+    }
+    else{
+        alert('You cant order more than 10 items')
+    }
+    total1.innerText =  `Total:${quantity*765}/-`;
+    showqauntity1.innerText = `${quantity} x 764`;
+ }
+
+ function decre1(){
+    const quantityText = document.getElementById("quantity-text1");
+    const total = document.getElementById("total1");
+    const showquantity = document.getElementById("showqauntity1")
+    let quantity = parseInt(quantityText.innerText);
+    if(quantity>1){
+    quantityText.innerText = --quantity;
+    }
+    else{
+        alert('You cant order less than 1 items')
+    }
+    total1.innerText =  `Total:${quantity*764}/-`;
+    showqauntity1.innerText = `${quantity} x 764`;
+   
+ }

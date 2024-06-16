ecd9b1f0e9d474dfc1e748079233ab580cb15463
Vaishugarudkar01 <vaishugarudkar2005@gmail.com>
2024-06-16 13:56:53 +0530

    footer added

---
+++ js/about.js
@@ -0,0 +1,26 @@
+function lipvc1(){
+    var obj = {"video": {
+        "value": "<iframe title='YouTube video player' type=\"text/html\" width='1000' height='500' src='https://www.youtube.com/embed/4t0vyJjLvmo?si=UMVxorSueED_gW1o' frameborder='0' allowFullScreen></iframe>"
+      }}
+      document.write(obj.video.value);
+    }
+
+
+    function lipvc2(){
+        var obj = {"video": {
+            "value": "<iframe title='YouTube video player' type=\"text/html\" width='1000' height='500' src='https://www.youtube.com/embed/vVqrM4ENTlE?si=7aBRhzuIsDtWBtfp' frameborder='0' allowFullScreen></iframe>"
+          }}
+          document.write(obj.video.value);
+        }
+
+
+        function lipvc3(){
+            var obj = {"video": {
+                "value": "<iframe title='YouTube video player' type=\"text/html\" width='1000' height='500' src=' https://www.youtube.com/embed/ZNPEgRIEkBY?si=_3LUC9cL8qGnAPUF' frameborder='0' allowFullScreen></iframe>"
+              }}
+              document.write(obj.video.value);
+            }
+    
+
+
+

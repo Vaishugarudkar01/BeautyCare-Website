451bee082eb80d79c75dd899007b9ef1e2b864d3
Vaishugarudkar01 <vaishugarudkar2005@gmail.com>
2024-06-16 18:06:29 +0530

    some changegs

---
+++ js/index.js
@@ -0,0 +1,16 @@
+function text1(){
+ 
+    let wallpaper = document.getElementById("main-img1");
+    wallpaper.src = '../images/slide6.webp';
+        
+}
+function text2(){
+ 
+    const wallpaper = document.getElementById("main-img2");
+    wallpaper.src = '../images/slide4.avif';
+}
+function text3(){
+ 
+    const wallpaper = document.getElementById("main-img3");
+    wallpaper.src = '../images/slide3.avif';
+}
---
+++ js/login.js
@@ -0,0 +1,17 @@
+function log(){
+    let username = document.getElementById("username");
+    let password = document.getElementById("password");
+     if(username.value == "")
+    {
+        alert("Please Enter Your Username")
+    }
+    else 
+    if(password.value == "")
+    {
+        alert("Please Enter Your Password")
+    }
+    else 
+     alert("Login successful");
+
+   
+}
